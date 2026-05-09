f52ab2987c57e1f76dd3658329d53970017f7535
xuexiaobo <625689128@qq.com>
2026-05-09 10:41:21 +0800

    添加初始数据

---
+++ app/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.xb.selfrichapp"
+    compileSdk {
+        version = release(36)
+    }
+
+    signingConfigs {
+        register("key") {
+            storeFile = file("../demo.jks")
+            storePassword = "as123456"
+            keyAlias = "key0"
+            keyPassword = "as123456"
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.xb.selfrichapp"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = signingConfigs.getByName("key")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+
+    implementation("com.squareup.okhttp3:okhttp:3.14.9")
+    implementation("io.reactivex.rxjava3:rxjava:3.1.4")
+    implementation("io.reactivex.rxjava3:rxandroid:3.0.0")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,4 +13,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "SelfRichApp"
-include ':app'
+include(":app")
